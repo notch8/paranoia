3adc81120421e60b18315e1605eb0f990a3410bf
Rob Kaufman <rob@notch8.com>
2016-09-13 13:56:36 -0700

    Update paranoia.rb
    remove default scope

--- lib/paranoia.rb
+++ lib/paranoia.rb
@@ -140,7 +140,8 @@ class ActiveRecord::Base
     class_attribute :paranoia_column
 
     self.paranoia_column = options[:column] || :deleted_at
-    default_scope { where(self.quoted_table_name + ".#{paranoia_column} IS NULL") }
+    
+    scope :not_deleted, where(self.quoted_table_name + ".#{paranoia_column} IS NULL")
 
     before_restore {
       self.class.notify_observers(:before_restore, self) if self.class.respond_to?(:notify_observers)
